14da19816c6e361b0576a0bd632c0ca465f107ca
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-10-12 22:25:33 +0900

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { New } from './pages/schedules/New';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container, NavbarBrand } from 'react-bootstrap';
 
 export function App() {
   return (
     <>
-      <Navbar></Navbar>
-      <BrowserRouter>
-        <Route exact path="/schedules/new" component={New} />
-      </BrowserRouter>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <NavbarBrand>全員集合！</NavbarBrand>
+        </Container>
+      </Navbar>
+      <Container>
+        <BrowserRouter>
+          <Route exact path="/schedules/new" component={New} />
+        </BrowserRouter>
+      </Container>
     </>
   );
 }
