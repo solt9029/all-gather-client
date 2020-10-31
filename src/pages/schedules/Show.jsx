35f1155ba5b2c532612bd5a729c9b4985755d8ac
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-10-31 13:30:17 +0900

    Add fetch schedule

--- src/pages/schedules/Show.jsx
+++ src/pages/schedules/Show.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
-import { createSchedule } from '../../utils';
+import { createSchedule, fetchSchedule } from '../../utils';
 import { useRouteMatch } from 'react-router-dom';
 
 export function Show() {
   const routeMatch = useRouteMatch();
+  const [schedule, setSchedule] = useState({
+    id: routeMatch.params.id,
+    title: '',
+    dates: [],
+  });
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetchSchedule(routeMatch.params.id);
+      setSchedule(response.data);
+    })();
+  }, [routeMatch]);
+
   console.log(routeMatch.params.id);
   return <div>show</div>;
 }
--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,7 @@ export const client = axios.create({ baseURL });
 export const createSchedule = async (data) => {
   return client.post('/schedules', data);
 };
+
+export const fetchSchedule = async (id) => {
+  return client.get(`/schedules/${id}`);
+};
